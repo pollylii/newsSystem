e7f4d1504e10f3b1d1ad89bb8bceae391a7db9a1
pollylilh <794211268@qq.com>
2022-06-17 18:39:00 +0800

    权限管理

--- src/views/sandbox/right-manage/RightList.js
+++ src/views/sandbox/right-manage/RightList.js
@@ -1,9 +1,99 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { Button, Table, Tag, Modal, notification } from 'antd'
+import axios from 'axios'
+import {
+    EditOutlined,
+    DeleteOutlined,
+    ExclamationCircleOutlined
+} from '@ant-design/icons';
 
+const { confirm } = Modal
 export default function RightList() {
+    const [dataSource, setdataSource] = useState([])
+    useEffect(() => {
+        axios.get("http://localhost:5000/rights?_embed=children").then(res => {
+            const dataList = res.data
+            dataList.forEach(item => {
+                if (item.children.length === 0) {
+                    item.children = ""
+                }
+            })
+            setdataSource(dataList)
+        })
+    }, [])
+
+    const columns = [
+        {
+            title: 'ID',
+            dataIndex: 'id',
+            width: 300,
+        },
+        {
+            title: '权限名称',
+            dataIndex: 'title',
+        },
+        {
+            title: '权限路径',
+            dataIndex: 'key',
+            render: (key) => {
+                return <Tag color="orange">{key}</Tag>
+            }
+        },
+        {
+            title: '操作',
+            width: 200,
+            render: (item) => {
+                return (
+                    <div>
+                        <Button type="primary" ghost shape="circle" icon={<EditOutlined />} />
+                        &nbsp;&nbsp;&nbsp;&nbsp;
+                        <Button type="dashed" danger shape="circle" icon={<DeleteOutlined />}
+                            onClick={() => confirmMethod(item)}
+                        />
+                    </div>
+                );
+            },
+        },
+    ];
+
+    const confirmMethod = (item) => {
+        confirm({
+            title: '你确定要删除?',
+            icon: <ExclamationCircleOutlined />,
+            // content: 'Some descriptions',
+            onOk() {
+                deleteMethod(item)
+            },
+            onCancel() {
+                notification.info({
+                    message: `提示`,
+                    description: '您已取消删除',
+                    placement: 'top',
+                });
+            },
+        });
+
+    }
+    //删除
+    const deleteMethod = (item) => {
+        // 当前页面同步状态 + 后端同步
+        if (item.grade === 1) {
+            setdataSource(dataSource.filter(data => data.id !== item.id))
+            axios.delete(`http://localhost:5000/rights/${item.id}`)
+        } else {
+            let list = dataSource.filter(data => data.id === item.rightId)
+            list[0].children = list[0].children.filter(data => data.id !== item.id)
+            setdataSource([...dataSource])
+            axios.delete(`http://localhost:5000/children/${item.id}`)
+        }
+    }
     return (
         <div>
-            RightList
+            <Table dataSource={dataSource} columns={columns}
+                pagination={{
+                    pageSize: 5,
+                }}
+            />;
         </div>
     )
 }
